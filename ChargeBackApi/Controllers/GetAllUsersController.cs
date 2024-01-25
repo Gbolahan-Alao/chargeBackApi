4c243117194960fb0e1033e4743e6eabff12481a
Alao <gbolahanlao@gmail.com>
2024-01-25 20:59:17 +0100

    Initial commit

---
+++ ChargeBackApi/Controllers/GetAllUsersController.cs
@@ -0,0 +1,25 @@
+﻿
+using ChargeBackAuthApi.Data;
+using ChargeBackAuthApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ChargeBackApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllUsersController : ControllerBase
+    {
+        private readonly IUserAccount _userAccount;
+        public GetAllUsersController(IUserAccount userAccount)
+        {
+            _userAccount = userAccount;
+        }
+        [HttpGet("GetUsers")]
+        public ActionResult<IEnumerable<ApplicationUser>> GetRegisteredUsers()
+        {
+            var users = _userAccount.GetRegisteredUsers();
+            return Ok(users);
+        }
+    }
+}
